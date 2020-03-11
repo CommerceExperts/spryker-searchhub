933fd6592e531a9c842aa33affc6f3aa790c7b5f
Siegfried Schuele <siegfried.schuele@commerce-experts.com>
2020-03-11 13:22:41 +0000

    update config.dist.php

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,4 +1,5 @@
 use SearchHub\Shared\SearchHubConstants;
+use Twig\Cache\FilesystemCache;
 
 // ---------- SearchHub
 /*
@@ -10,7 +11,6 @@ use SearchHub\Shared\SearchHubConstants;
  */
 $config[SearchHubConstants::SMARTQUERY_ENDPOINT] = 'https://test.searchhub.io/smartquery/v1/demo/spryker';
 
-// replace 'new CompletionResultFormatterPlugin(),' with 'new SearchHubSuggestResultFormatterPlugin(),' in \Pyz\Client\Catalog\CatalogDependencyProvider::createSuggestionResultFormatterPlugins
 /*
 * in order to activate SearchHub Suggest
 * replace 'new CompletionResultFormatterPlugin(),' with 'new SearchHubSuggestResultFormatterPlugin(),' in \Pyz\Client\Catalog\CatalogDependencyProvider::createSuggestionResultFormatterPlugins
@@ -20,3 +20,12 @@ $config[SearchHubConstants::SMARTQUERY_ENDPOINT] = 'https://test.searchhub.io/sm
 $config[SearchHubConstants::SMARTSUGGEST_ENDPOINT] = 'https://test.searchhub.io/smartsuggest/v1/demo/spryker';
 
 $config[SearchHubConstants::REQUEST_TIMEOUT] = '1000';
+
+$config[SearchHubConstants::USE_SAAS_MODE] = false;
+
+//the following searchhub configuration items are only needed if USE_SAAS_MODE = false
+$config[SearchHubConstants::API_KEY] = "request your api key from info@commerce-experts.com";
+$config[SearchHubConstants::MAPPING_QUERIES_ENDPOINT] = "https://query.searchhub.io/mappingData?tenant=demo.spryker";
+$config[SearchHubConstants::MAPPING_SUGGESTS_ENDPOINT] = "https://query.searchhub.io/suggest/data?tenant=demo.spryker";
+$config[SearchHubConstants::MAPPING_CACHE] = new FilesystemCache(sprintf('%s/data/cache/searchhub',APPLICATION_ROOT_DIR));
+$config[SearchHubConstants::MAPPING_CACHE_TTL] = "600";
