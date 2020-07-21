d5ad157d82139157c346fee79f74df9e460bc8ac
Siegfried Schuele <siegfried.schuele@commerce-experts.com>
2020-07-21 12:05:14 +0000

    add legacy Plugins for spryker/search < 8.4.0

---
+++ src/SearchHub/Client/Plugin/QueryExpander/SearchHubLegacyQueryExpanderPlugin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SearchHub\Client\Plugin\QueryExpander;
+
+use SearchHub\Client\SearchHubFactory;
+use SearchHub\Client\SearchHubRequest;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Client\Search\Dependency\Plugin\QueryExpanderPluginInterface;
+use Spryker\Client\Search\Dependency\Plugin\QueryInterface;
+use Spryker\Shared\Log\LoggerTrait;
+
+class SearchHubLegacyQueryExpanderPlugin extends AbstractPlugin implements QueryExpanderPluginInterface
+{
+
+    use LoggerTrait;
+
+    /**
+     * @param QueryInterface $searchQuery
+     * @param array $requestParameters
+     *
+     * @return QueryInterface
+     */
+    public function expandQuery(QueryInterface $searchQuery, array $requestParameters = [])
+    {
+        $searchHubClient = $this->factory()->getSearchHubClient();
+        $searchHubRequest = new SearchHubRequest(trim(strtolower($searchQuery->getSearchString())));
+        $searchHubRequest = $searchHubClient->optimizeQuery($searchHubRequest);
+        $optimizedQuery = $searchHubRequest->getSearchQuery();
+        if ($searchQuery->getSearchString() !== $optimizedQuery) {
+            $this->getLogger()->info("searchhub optimized query [" . $searchQuery->getSearchString() . "] -> [" . $optimizedQuery . "]");
+        }
+        $searchQuery->setSearchString($optimizedQuery);
+        return $searchQuery;
+    }
+
+    /**
+     * @return SearchHubFactory
+     */
+    protected function factory(): SearchHubFactory
+    {
+        return new SearchHubFactory();
+    }
+
+}
---
+++ src/SearchHub/Client/Plugin/QueryExpander/SearchStrategyOptimizerLegacyQueryExpanderPlugin.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace SearchHub\Client\Plugin\QueryExpander;
+
+use Elastica\Query\MultiMatch;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Client\Search\Dependency\Plugin\QueryExpanderPluginInterface;
+use Spryker\Client\Search\Dependency\Plugin\QueryInterface;
+use Spryker\Shared\Log\LoggerTrait;
+
+class SearchStrategyOptimizerLegacyQueryExpanderPlugin extends AbstractPlugin implements QueryExpanderPluginInterface
+{
+    use LoggerTrait;
+
+    const MUST = 'must';
+    const QUERY = 'query';
+    const TYPE = 'type';
+
+    /**
+     * @param QueryInterface $searchQuery
+     * @param array $requestParameters
+     *
+     * @return QueryInterface
+     */
+    public function expandQuery(QueryInterface $searchQuery, array $requestParameters = [])
+    {
+        $coreQuery = $searchQuery->getSearchQuery()->getParams(){self::QUERY}->getParams();
+        $optimized = false;
+        if (
+            is_array($searchQuery->getSearchQuery()->getParams())
+            && isset($searchQuery->getSearchQuery()->getParams(){self::QUERY})
+            && isset($coreQuery[self::MUST])
+            && is_array($coreQuery[self::MUST])) {
+
+            $mustQueries = $coreQuery[self::MUST];
+            for ($i = 0; $i < sizeof($mustQueries); $i++) {
+                $param = $mustQueries[$i];
+                if ($param instanceof MultiMatch && $param->hasParam(self::TYPE) && $param->getParam(self::TYPE) === MultiMatch::TYPE_CROSS_FIELDS) {
+                    $param->setParam(self::TYPE, MultiMatch::TYPE_MOST_FIELDS);
+                    $coreQuery[self::MUST][$i] = $param;
+                    $optimized = true;
+                    break;
+                }
+            }
+            $searchQuery->getSearchQuery()->getParams(){self::QUERY}->setParams($coreQuery);
+        }
+        if (!$optimized) {
+            $this->getLogger()->info("SearchStrategyOptimizerLegacyQueryExpanderPlugin failed to optimize search strategy. Maybe you have plugged me in too late in CatalogDependencyProvider?");
+        }
+        return $searchQuery;
+    }
+}
